705a202fb1c2915e0c452a82c7d719d999ffb89a
lishan <895977304@qq.com>
2019-04-09 23:58:04 +0800

    。。。提交6

--- app/src/main/java/com/kiwi/phonelive/activity/community/Act_CommunityDetails.java
+++ app/src/main/java/com/kiwi/phonelive/activity/community/Act_CommunityDetails.java
@@ -5,11 +5,15 @@ import android.view.WindowManager;
 
 import com.kiwi.phonelive.R;
 import com.kiwi.phonelive.activity.AbsActivity;
+import com.kiwi.phonelive.custom.ViewPagerIndicator;
+import com.kiwi.phonelive.views.AbsMainChildTopViewHolder;
 
 /**
  * 社区详情页
  */
 public class Act_CommunityDetails extends AbsActivity {
+    private ViewPagerIndicator indicator;
+
     @Override
     protected int getLayoutId() {
         return R.layout.act_communitydetails;
@@ -18,7 +22,20 @@ public class Act_CommunityDetails extends AbsActivity {
     @Override
     protected void main() {
         if (Build.VERSION.SDK_INT >= 21) {
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS); }
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+        initView();
+        initData();
+    }
+
+    public void initView() {
+        indicator = findViewById(R.id.indicator);
+
+    }
+
+    public void initData() {
+
+
     }
 
 }
